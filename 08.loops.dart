b8a57741b1f3aa5fd2e6c7e78bda9256f0fa3b37
priyankraychura <priyankraychura@gmail.com>
2024-11-24 17:15:08 +0530

    loops learned

---
+++ 08.loops.dart
@@ -0,0 +1,16 @@
+void main() {
+  for (int i = 0; i < 10; i++) {
+    print("Hello world");
+  }
+
+  int num = 10;
+
+  // do {
+  //   print("No is $num");
+  // } while (num < 50);
+
+  while (num < 50) {
+    print("No is $num");
+    num++;
+  }
+}
